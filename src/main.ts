f7ddd9b98cfe82eb01257df8416a08bd26ec861e
Luiz Rocco <luizgustavorocco@gmail.com>
2023-01-30 01:05:35 -0300

    Changed port listened and base swagger route

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,8 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('/', app, document);
 
-  await app.listen(3000);
+  await app.listen(6438);
 }
 bootstrap();
